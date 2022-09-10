e60dedfead5b5ac936f3d60b102129e98a545e54
Krishna Agarwal <dmkrishna.agarwal@gmail.com>
2022-09-10 22:38:45 +0530

    Revert "Update test_unittest1.py"

--- test_unittest1.py
+++ test_unittest1.py
@@ -6,3 +6,8 @@ class Test_TestPython(ut.TestCase):
 
     def test_another(self):
         self.assertEqual(100, 100)
+
+    def test_krishna(self):
+        self.assertEqual(200, 67)
+    def test_krishna1(self):
+        self.assertEqual(200, 67)
